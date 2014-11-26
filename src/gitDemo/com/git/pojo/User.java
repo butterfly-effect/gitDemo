ca4804a12fd7950d0f8c8851fdb1b3eeb0045ba4
butterfly-effect <lcging@163.com>
2014-11-26 09:20:55 +0800

    first submit

---
+++ src/gitDemo/com/git/pojo/User.java
@@ -0,0 +1,42 @@
+package gitDemo.com.git.pojo;
+
+public class User {
+
+	private String name;
+	private String age;
+	private String gender;
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}

784553faf6fe950285ea040f2980e7afd1747bab
butterfly-effect <lcging@163.com>
2014-12-04 13:15:47 +0800

    f

---
+++ src/gitDemo/com/git/pattern/template_method/AClass.java
@@ -0,0 +1,9 @@
+package gitDemo.com.git.pattern.template_method;
+
+public class AClass extends BaseTemplate {
+    @Override
+    public void doOtherThing() {
+        System.out.println("AClassImpl.doOtherThing");
+    }
+}
+
